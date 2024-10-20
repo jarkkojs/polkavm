9d665fabdb5eec421d923fc8625cd682e9c6b9a3
Jarkko Sakkinen <jarkko@parity.io>
2024-10-20 19:59:03 +0300

    guest-programs: fixup build-examples.sh for the new toolchain
    Signed-off-by: Jarkko Sakkinen <jarkko@parity.io>

--- guest-programs/build-examples.sh
+++ guest-programs/build-examples.sh
@@ -5,20 +5,26 @@ cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 source ../ci/jobs/detect-or-install-riscv-toolchain.sh
 
-if [ "${RV32E_TOOLCHAIN:-}" == "" ]; then
-    echo "ERROR: rv32e toolchain is missing; PolkaVM binaries can't be built!"
-    exit 1
-fi
-
 function build_example () {
     output_path="output/$1.polkavm"
-    current_dir=$(pwd)
 
     echo "> Building: '$1' (-> $output_path)"
-    RUSTFLAGS="-C target-feature=+c -C relocation-model=pie -C link-arg=--emit-relocs -C link-arg=--unique --remap-path-prefix=$(pwd)= --remap-path-prefix=$HOME=~" cargo build -q --release --bin $1 -p $1
-    cd ..
-    cargo run -q -p polkatool link --run-only-if-newer -s guest-programs/target/riscv32ema-unknown-none-elf/release/$1 -o guest-programs/$output_path
-    cd $current_dir
+
+    local target="riscv32emac-unknown-none-polkavm"
+    local extra_flags="-C target-feature=+c -C relocation-model=pie -C link-arg=--emit-relocs -C link-arg=--unique --remap-path-prefix=$(pwd)= --remap-path-prefix=$HOME=~"
+
+    RUSTFLAGS="$extra_flags" cargo build  \
+        -Z build-std=core,alloc \
+        --target "$PWD/$target.json" \
+        -q --release --bin $1 -p $1
+
+    pushd ..
+
+    cargo run -q -p polkatool link \
+        --run-only-if-newer -s guest-programs/target/$target/release/$1 \
+        -o guest-programs/$output_path
+
+    popd
 }
 
 build_example "example-hello-world"
